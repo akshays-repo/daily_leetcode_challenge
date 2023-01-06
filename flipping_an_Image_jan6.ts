b6400ffae406b04d45660f3fddad93495c1eeb53
akshays-bw <akshay.s@brainwired.in>
2023-01-06 09:42:39 +0530

    init : flipping_an_Image_jan6.ts

---
+++ flipping_an_Image_jan6.ts
@@ -0,0 +1,26 @@
+function flipAndInvertImage(image: number[][]): number[][] {
+  let result: number[][] = [];
+
+  for (let i = 0; i < image.length; i++) {
+    let newArray: number[] = [];
+
+    for (let j = image[i].length - 1; j >= 0; j--) {
+      if (image[i][j] === 0) {
+        newArray.push(1);
+      } else {
+        newArray.push(0);
+      }
+    }
+
+    result.push(newArray);
+  }
+
+  return result;
+}
+console.log(
+  flipAndInvertImage([
+    [1, 1, 0],
+    [1, 0, 1],
+    [0, 0, 0],
+  ])
+);
