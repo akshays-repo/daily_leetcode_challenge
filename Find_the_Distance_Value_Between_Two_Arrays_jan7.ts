aa9c568084e1a72259c832a5bec9bbad11338703
akshays-bw <akshay.s@brainwired.in>
2023-01-09 09:59:01 +0530

    init: jan 7

---
+++ Find_the_Distance_Value_Between_Two_Arrays_jan7.ts
@@ -0,0 +1,27 @@
+function findTheDistanceValue(
+  arr1: number[],
+  arr2: number[],
+  d: number
+): number {
+  let distance: number = 0;
+
+  for (let i = 0; i < arr1.length; i++) {
+    const element1 = arr1[i];
+
+    for (let j = 0; j < arr2.length; j++) {
+      const element2 = arr2[j];
+
+      if (element1 !== element2) {
+        let op =
+          element1 > element2 ? element1 - element2 : element2 - element1;
+        if (op <= d) {
+          distance = distance + 1;
+          console.log(`${element1} - ${element2} = ${op}`);
+        }
+      }
+    }
+  }
+  return distance;
+}
+
+console.log(findTheDistanceValue([4, 5, 8], [4, 5, 8], 3));
