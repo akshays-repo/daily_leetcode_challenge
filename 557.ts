d9dbc18595197b364731db9eda5d435daab5ec45
akshays-repo <akshayshan28@gmail.com>
2023-01-12 09:53:54 +0530

    chore: accepted

--- 557.ts
+++ 557.ts
@@ -2,10 +2,36 @@
 // sentence while still preserving whitespace and initial word order.
 
 // Example 1:
-
 // Input: s = "Let's take LeetCode contest"
 // Output: "s'teL ekat edoCteeL tsetnoc"
-// Example 2:
 
+// Example 2:
 // Input: s = "God Ding"
 // Output: "doG gniD"
+
+// status: Completed
+
+// Runtime: 87 ms
+// Beats: 73.20%
+// Memory: 49.3 MB
+// Beats: 66.67%
+
+function reverseWords(s: string): string {
+  let op: string = "";
+
+  const sArr = s.split(" ");
+
+  for (let index = 0; index < sArr.length; index++) {
+    const element = sArr[index];
+    if (index != 0) {
+      op = op + " ";
+    }
+    for (let j = element.length - 1; j >= 0; j--) {
+      op = op + element[j];
+    }
+  }
+
+  return op;
+}
+
+console.log(reverseWords("God Ding"));
