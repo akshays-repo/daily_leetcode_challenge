1ba102bc7b877020fb6911482e0d0cbf986600a6
akshays-repo <akshayshan28@gmail.com>
2023-01-16 09:51:40 +0530

    init: jan 16

---
+++ 1684.ts
@@ -0,0 +1,13 @@
+// 1684. Count the Number of Consistent Strings
+
+// You are given a string allowed consisting of distinct characters and
+// an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+// Return the number of consistent strings in the array words.
+
+// Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+// Output: 2
+// Explanation: Strings "aaab" and "baa" are consistent since they only contain c
+// characters 'a' and 'b'.
+
+function countConsistentStrings(allowed: string, words: string[]): number {}
